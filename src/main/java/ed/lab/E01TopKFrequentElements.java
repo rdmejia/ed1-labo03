c5eb49e771c05150dd8246dfde7d28d51db8b5be
javu2 <jemonje@correo.url.edu.gt>
2025-05-15 17:47:51 -0600

    Push test

--- src/main/java/ed/lab/E01TopKFrequentElements.java
+++ src/main/java/ed/lab/E01TopKFrequentElements.java
@@ -1,7 +1,31 @@
 package ed.lab;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+
 public class E01TopKFrequentElements {
     public int[] topKFrequent(int[] nums, int k) {
-        return null;
+        int n = nums.length;
+        int[] ans = new int[k];
+        Map<Integer, Integer> map = new HashMap<>();
+        PriorityQueue<Map.Entry<Integer, Integer>> minHeap = new PriorityQueue<>((a, b) -> Integer.compare(a.getValue(), b.getValue()));
+
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            minHeap.offer(entry);
+            if (minHeap.size() > k) {
+                minHeap.poll();
+            }
+        }
+
+        for (int i = k - 1; i >= 0; i--) {
+            ans[i] = minHeap.poll().getKey();
+        }
+
+        return ans;
     }
 }
